103c2bbc739719146bb8d4e7a51093bef99b93aa
skylock <emanuil.copil@gmail.com>
2019-03-08 14:03:21 +0200

    Create event thumbnail component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,16 +2,22 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'events-list',
-  templateUrl: './events-list.component.html'
+  template: `
+    <div>
+      <h1>Upcoming Angular events</h1>
+      <hr/>
+      <event-thumbnail [event]="event1"></event-thumbnail>
+    </div>
+  `
 })
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Connect',
     date: '9/26/2036',
     time: '10:00 am',
     price: 599.99,
-    imageUrl: '/assets/images/angularconnect-shield.png',
+    imageUrl: '/app/assets/images/angularconnect-shield.png',
     location: {
       address: '1057 DT',
       city: 'London',
