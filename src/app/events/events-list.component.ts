1c04406b4ea05388df5953055cac8c52a4de5b9c
skylock <emanuil.copil@gmail.com>
2019-03-08 14:48:15 +0200

    Testing event propagation from child to parent component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import {Component} from "@angular/core";
     <div>
       <h1>Upcoming Angular events</h1>
       <hr/>
-      <event-thumbnail [event]="event1"></event-thumbnail>
+      <event-thumbnail [event]="event1" (eventClick)="handleChildEventClick($event)"></event-thumbnail>
     </div>
   `
 })
@@ -23,5 +23,9 @@ export class EventsListComponent {
       city: 'London',
       country: 'England'
     }
+  }
+
+  handleChildEventClick(data: any) {
+    alert(data);
   }
 }
