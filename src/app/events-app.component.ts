ac08a5f46616b8e22ffceb6de1b705c820170c96
skylock <emanuil.copil@gmail.com>
2019-03-08 10:51:07 +0200

    Rename default component selector

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'events-app',
   template: '<h2>Hello World!</h2>',
 })
 export class EventsAppComponent {
