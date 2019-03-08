e91fe02cacea06b2a1314955d3025cd29f9bd6f1
skylock <emanuil.copil@gmail.com>
2019-03-08 13:18:33 +0200

    Add first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import {EventsListComponent} from "./events/events-list.component";
 
 @NgModule({
   imports: [
     BrowserModule
+
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent
   ],
   providers: [],
   bootstrap: [EventsAppComponent]
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'events-list',
+  template: `
+    <div>
+      <h3>Hello from events list !</h3>
+    </div>
+  `
+})
+export class EventsListComponent {
+
+}
