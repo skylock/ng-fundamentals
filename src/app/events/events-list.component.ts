71d6c0654535a7607c3662fac5cd475d9b943777
skylock <emanuil.copil@gmail.com>
2019-03-22 09:20:12 +0200

    Style events displayed

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,11 @@ import {Component} from "@angular/core";
     <div>
       <h1>Upcoming Angular events</h1>
       <hr/>
-      <event-thumbnail *ngFor="let event of events" [event]="event"></event-thumbnail>
+      <div class="row">
+        <div class="col-md-5" *ngFor="let event of events">
+          <event-thumbnail [event]="event"></event-thumbnail>    
+        </div>
+      </div>
     </div>
   `
 })
