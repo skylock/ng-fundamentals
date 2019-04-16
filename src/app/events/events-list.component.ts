65dfcbc69e6afa38a0ba4f8bea94ecc8aefeb53f
skylock <emanuil.copil@gmail.com>
2019-04-16 11:03:56 +0300

    Add toast messages

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 
+declare let toastr;
+
 @Component({
   selector: 'ngf-events-list',
   templateUrl: './events-list.component.html'
@@ -14,5 +16,9 @@ export class EventsListComponent implements OnInit {
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName: any) {
+    toastr.success(eventName);
   }
 }
