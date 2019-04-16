7c52263d3cf01b81c3d34e85e445897ee5477ed6
skylock <emanuil.copil@gmail.com>
2019-04-16 10:54:49 +0300

    Fetch data on component initialization

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 
 @Component({
   selector: 'ngf-events-list',
   templateUrl: './events-list.component.html'
 })
-export class EventsListComponent {
+export class EventsListComponent implements OnInit {
 
-  private events: any;
+  private events: any[];
 
   constructor(private eventService: EventService) {
-    this.events = eventService.getEvents();
+  }
+
+  ngOnInit() {
+    this.events = this.eventService.getEvents();
   }
 }
