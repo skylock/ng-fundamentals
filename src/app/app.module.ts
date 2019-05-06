94bfd05ae2e6e772c811f101c6ecc3cd5c2a4687
skylock <emanuil.copil@gmail.com>
2019-05-06 11:42:39 +0300

    Ordered imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventAddressComponent } from './events/event-address.component';
-import { NavBarComponent } from './nav/nav-bar.component';
-import { EventService } from './events/shared/event.service';
-import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { appRoutes } from './routes';
 import { RouterModule } from '@angular/router';
-import { CreateEventComponent } from './events/create-event.component';
+import { ToastrService } from './common/toastr.service';
 import { Error404Component } from './errors/404.component';
+import { EventsAppComponent } from './events-app.component';
+import { CreateEventComponent } from './events/create-event.component';
+import { EventAddressComponent } from './events/event-address.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
+import { NavBarComponent } from './nav/nav-bar.component';
+import { appRoutes } from './routes';
 
 @NgModule({
   imports: [
@@ -29,7 +30,7 @@ import { Error404Component } from './errors/404.component';
     Error404Component,
     NavBarComponent
   ],
-  providers: [EventService, ToastrService],
+  providers: [EventService, ToastrService, EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
