d2682cd78243b3b6b4e0cf9359aad240c06eeb87
skylock <emanuil.copil@gmail.com>
2019-05-07 13:15:18 +0300

    Enable user to leave profile page on cancel

--- src/app/events/events-list-resolver.service.ts
+++ src/app/events/events-list-resolver.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
-import { EventService } from './shared/event.service';
 import { map } from 'rxjs/operators';
 import { CreateEventComponent } from './create-event.component';
+import { EventService } from './shared/event.service';
 
 @Injectable()
 export class EventListResolver implements Resolve<any> {
