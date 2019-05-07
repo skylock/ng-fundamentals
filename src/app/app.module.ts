5da0a1d99258f3e5dbd328be8b87816e95c6b27c
skylock <emanuil.copil@gmail.com>
2019-05-07 13:11:00 +0300

    Barrel imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
 import { Error404Component } from './errors/404.component';
+import { checkDirtyState, CreateEventComponent, EventAddressComponent, EventDetailsComponent, EventListResolver,
+          EventRouteActivator, EventService, EventsListComponent, EventThumbnailComponent } from './events';
 import { EventsAppComponent } from './events-app.component';
-import { CreateEventComponent } from './events/create-event.component';
-import { EventAddressComponent } from './events/event-address.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/nav-bar.component';
 import { appRoutes } from './routes';
-import { checkDirtyState, EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [
