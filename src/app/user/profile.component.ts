c1a24409ae6ee8ce34dc4492317d688c7efd117a
skylock <emanuil.copil@gmail.com>
2019-05-09 12:08:37 +0300

    Add reactive form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
-    </div>
-  `,
+  templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   isDirty = true;
-  constructor(private router: Router) {
+  profileForm: FormGroup;
 
+  constructor(private router: Router,
+              private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    const firstName = new FormControl(this.authService.currentUser.firstName);
+    const lastName = new FormControl(this.authService.currentUser.lastName);
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
   }
 
   cancel() {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user.routes';
 
 @NgModule({
-  imports : [
+  imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild(userRoutes)
+    RouterModule.forChild(userRoutes),
+    ReactiveFormsModule
   ],
   declarations: [
     LoginComponent,
